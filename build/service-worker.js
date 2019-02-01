5cd4e9547a2643b4fcdd889450df419b4aa73377
Vladimir Davidchik <Vladimir.Davidchik@cortlex.com>
2019-02-01 12:37:53 +0300

    test build

---
+++ build/service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/react-redux-router/precache-manifest.6b6eb5c016f6ccc9d9c985a6b142cbdf.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/react-redux-router/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
--- src/index.js
+++ src/index.js
@@ -4,18 +4,28 @@ import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 
 import './index.css';
-import Router from './routes';
+// import Router from './routes';
+import App from './containers/App';
 import store from './redux/store';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
   <BrowserRouter>
     <Provider store={store}>
-      <Router />
+      <App />
     </Provider>
   </BrowserRouter>,
   document.getElementById('root'),
 );
+
+// ReactDOM.render(
+//   <BrowserRouter>
+//     <Provider store={store}>
+//       <Router />
+//     </Provider>
+//   </BrowserRouter>,
+//   document.getElementById('root'),
+// );
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
