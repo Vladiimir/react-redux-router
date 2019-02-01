443248afddf2ee6ce1dadfc5990044e00a44ed0c
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-02-01 16:32:18 +0000

    Deploy Vladiimir/react-redux-router to github.com/Vladiimir/react-redux-router.git:gh-pages

---
+++ precache-manifest.2e8191b7c19a04f79caaad8473172a5b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "c5b0da42c6baf6532ce8",
+    "url": "/react-redux-router/static/css/main.97dd9b3e.chunk.css"
+  },
+  {
+    "revision": "c5b0da42c6baf6532ce8",
+    "url": "/react-redux-router/static/js/main.c5b0da42.chunk.js"
+  },
+  {
+    "revision": "c88bb57b63fabee772fc",
+    "url": "/react-redux-router/static/js/1.c88bb57b.chunk.js"
+  },
+  {
+    "revision": "a46d3945987587c97961",
+    "url": "/react-redux-router/static/js/runtime~main.a46d3945.js"
+  },
+  {
+    "revision": "51a5d8e1a2dd1899eb8ef5518e71c7ce",
+    "url": "/react-redux-router/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.e8062d68d39cac55b748fbbc0a7646b1.js"
+  "/react-redux-router/precache-manifest.2e8191b7c19a04f79caaad8473172a5b.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/react-redux-router/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
